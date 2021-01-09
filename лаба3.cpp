f7f8494be87811a2358a11619b68e0af3330a224
lavorenko <75750187+lavorenko@users.noreply.github.com>
2021-01-09 13:26:34 +0300

    Add files via upload

---
+++ лаба3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, k10, k5, k2, k1, k;
+	cout << "n = ", cin >> n, cout << "\n";
+	k = 0;
+	for (k10 = 0; k10 <= n / 10; k10++)
+		for (k5 = 0; k5 <= (n - 10 * k10) / 5; k5++)
+			for (k2 = 0; k2 <= (n - 10 * k10 - 5 * k5) / 2; k2++) {
+				k1 = n - 10 * k10 - 5 * k5 - 2 * k2;
+				cout << k10 << " " << k5 << " " << k2 << " " << k1 << "\n";
+				k = k + 1;
+			}
+	cout << "k = " << k;
+	return 0;
+}
